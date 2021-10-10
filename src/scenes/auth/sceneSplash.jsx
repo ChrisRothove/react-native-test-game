bd29ea893c8e25175633c3a569bfdd6f2cd63fb2
featherwalk <vacantorange@gmail.com>
2021-10-10 13:45:31 -0500

    added user actions and reducer for login

--- src/scenes/auth/sceneSplash.jsx
+++ src/scenes/auth/sceneSplash.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Text } from 'react-native'
+import PropTypes from 'prop-types'
 import DefaultPage from '../../components/DefaultPage'
 
-function SceneSplash() {
+const SceneSplash = ({ navigation }) => {
+    useEffect(() => {
+        setTimeout(() => {
+            navigation.navigate('Login')
+        }, 2000)
+    }, [])
     return (
         <DefaultPage>
             <Text>Splash</Text>
         </DefaultPage>
     )
+}
+
+SceneSplash.propTypes = {
+    navigation: PropTypes.object.isRequired,
 }
 
 export default SceneSplash
