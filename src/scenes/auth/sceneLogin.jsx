015483e8731c67713d4376d7cd5c882e8a18dcc4
featherwalk <vacantorange@gmail.com>
2021-10-12 22:34:23 -0500

    added BackgroundPage and added bg image to Login

--- src/scenes/auth/sceneLogin.jsx
+++ src/scenes/auth/sceneLogin.jsx
@@ -1,21 +1,22 @@
-import React, { useState } from 'react'
-import { Text, TextInput, Button } from 'react-native'
-import { connect } from 'react-redux'
-import PropTypes from 'prop-types'
-import DefaultPage from '../../components/global/layout/DefaultPage'
-import { userLogin } from '../../redux/actions/user.actions'
+import React, { useState } from "react";
+import { Text, TextInput, Button } from "react-native";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { userLogin } from "../../redux/actions/user.actions";
+import BackgroundPage from "../../components/global/layout/BackgroundPage";
+import backgroundImage from "../../../assets/bg_abstract_01.jpg";
 
 const SceneLogin = ({ loginUser, isUserLoggedIn, storedUserName }) => {
-    const [name, setName] = useState('')
+    const [name, setName] = useState("");
 
     return (
-        <DefaultPage>
+        <BackgroundPage background={backgroundImage}>
             <Text>Login</Text>
             <TextInput
                 style={{
                     height: 50,
                     width: 200,
-                    borderColor: 'gray',
+                    borderColor: "gray",
                     borderWidth: 3,
                     padding: 10,
                     margin: 10,
@@ -23,30 +24,30 @@ const SceneLogin = ({ loginUser, isUserLoggedIn, storedUserName }) => {
                 onChangeText={(text) => setName(text)}
                 value={name}
             />
-            <Button title="Press me" onPress={() => loginUser(name || 'Anon')} />
+            <Button title="Press me" onPress={() => loginUser(name || "Anon")} />
 
             {isUserLoggedIn ? <Text>{`Welcome, ${storedUserName}!`}</Text> : undefined}
-        </DefaultPage>
-    )
-}
+        </BackgroundPage>
+    );
+};
 
 SceneLogin.defaultProps = {
-    storedUserName: '',
-}
+    storedUserName: "",
+};
 
 SceneLogin.propTypes = {
     loginUser: PropTypes.func.isRequired,
     isUserLoggedIn: PropTypes.bool.isRequired,
     storedUserName: PropTypes.string,
-}
+};
 
 const mapStateToProps = (state) => ({
     storedUserName: state.user.name,
     isUserLoggedIn: state.user.isLoggedIn,
-})
+});
 
 const mapDispatchToProps = (dispatch) => ({
     loginUser: (name) => dispatch(userLogin({ name })),
-})
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(SceneLogin)
+export default connect(mapStateToProps, mapDispatchToProps)(SceneLogin);
