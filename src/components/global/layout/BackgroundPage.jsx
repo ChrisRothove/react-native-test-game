3f61220e14657cd04d2bbb15e993afa52ea6aae8
featherwalk <vacantorange@gmail.com>
2021-10-24 18:56:06 -0500

    added new Login Background and adjusted login screen

--- src/components/global/layout/BackgroundPage.jsx
+++ src/components/global/layout/BackgroundPage.jsx
@@ -1,26 +1,26 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { StyleSheet, ImageBackground } from 'react-native'
+import React from "react";
+import PropTypes from "prop-types";
+import { StyleSheet, ImageBackground } from "react-native";
 
 const BackgroundPage = ({ background, children }) => (
     <ImageBackground source={background} style={styles.imageBackground}>
         {children}
     </ImageBackground>
-)
+);
 
 const styles = StyleSheet.create({
     imageBackground: {
         flex: 1,
-        flexDirection: 'column',
-        resizeMode: 'cover',
-        alignItems: 'center',
-        justifyContent: 'center',
+        flexDirection: "column",
+        resizeMode: "cover",
+        alignItems: "center",
+        justifyContent: "top",
     },
-})
+});
 
 BackgroundPage.propTypes = {
     background: PropTypes.number.isRequired,
     children: PropTypes.node.isRequired,
-}
+};
 
-export default BackgroundPage
+export default BackgroundPage;
