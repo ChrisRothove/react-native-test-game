8b501f32d2dfe148d6fbd52092786713a0d3b613
ChrisRothove <vacantorange@gmail.com>
2021-10-24 19:07:54 -0500

    fixed incorrect styling on BackgroundPage

--- src/scenes/auth/sceneLogin.jsx
+++ src/scenes/auth/sceneLogin.jsx
@@ -1,14 +1,16 @@
-import React, { useState } from "react";
-import { View, Text, TextInput, StyleSheet } from "react-native";
-import { connect } from "react-redux";
-import PropTypes from "prop-types";
-import { userLogin } from "../../redux/actions/user.actions";
-import BackgroundPage from "../../components/global/layout/BackgroundPage";
-import backgroundImage from "../../../assets/PropellerFieldsSplash.png";
-import TextButton from "../../components/global/ui/TextButton";
+import React, { useState } from 'react'
+import {
+    View, Text, TextInput, StyleSheet,
+} from 'react-native'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import { userLogin } from '../../redux/actions/user.actions'
+import BackgroundPage from '../../components/global/layout/BackgroundPage'
+import backgroundImage from '../../../assets/PropellerFieldsSplash.png'
+import TextButton from '../../components/global/ui/TextButton'
 
 const SceneLogin = ({ loginUser, isUserLoggedIn, storedUserName }) => {
-    const [name, setName] = useState("");
+    const [name, setName] = useState('')
 
     return (
         <BackgroundPage background={backgroundImage}>
@@ -22,60 +24,60 @@ const SceneLogin = ({ loginUser, isUserLoggedIn, storedUserName }) => {
                 <TextButton
                     title="Login"
                     type="secondary"
-                    onPress={() => loginUser(name || "Anon")}
+                    onPress={() => loginUser(name || 'Anon')}
                 />
 
                 {isUserLoggedIn ? <Text>{`Welcome, ${storedUserName}!`}</Text> : undefined}
             </View>
         </BackgroundPage>
-    );
-};
+    )
+}
 
 const styles = StyleSheet.create({
     container: {
-        borderColor: "gray",
-        backgroundColor: "white",
+        borderColor: 'gray',
+        backgroundColor: 'white',
         borderWidth: 3,
         padding: 40,
         borderRadius: 10,
-        alignItems: "center",
-        justifyContent: "center",
+        alignItems: 'center',
+        justifyContent: 'center',
         marginTop: 110,
     },
     headline: {
-        fontWeight: "bold",
+        fontWeight: 'bold',
         fontSize: 23,
     },
     input: {
         height: 50,
         width: 200,
-        borderColor: "gray",
-        backgroundColor: "lightgray",
+        borderColor: 'gray',
+        backgroundColor: 'lightgray',
         borderWidth: 3,
         borderRadius: 3,
         padding: 10,
         margin: 10,
         fontSize: 18,
     },
-});
+})
 
 SceneLogin.defaultProps = {
-    storedUserName: "",
-};
+    storedUserName: '',
+}
 
 SceneLogin.propTypes = {
     loginUser: PropTypes.func.isRequired,
     isUserLoggedIn: PropTypes.bool.isRequired,
     storedUserName: PropTypes.string,
-};
+}
 
 const mapStateToProps = (state) => ({
     storedUserName: state.user.name,
     isUserLoggedIn: state.user.isLoggedIn,
-});
+})
 
 const mapDispatchToProps = (dispatch) => ({
     loginUser: (name) => dispatch(userLogin({ name })),
-});
+})
 
-export default connect(mapStateToProps, mapDispatchToProps)(SceneLogin);
+export default connect(mapStateToProps, mapDispatchToProps)(SceneLogin)
